f71f90ea119d4c6c8427e40cf46a7419e9973f1d
RoyNyaga <nyagaandreroy@gmail.com>
2023-12-16 23:50:21 +0100

    create teacher devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :teachers
   get 'pages/home'
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
