e04707d63a4a1e9e07b53e194b4c24d3029353f3
RoyNyaga <nyagaandreroy@gmail.com>
2023-12-17 00:12:13 +0100

    add configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate_teacher!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[phone_number first_name other_names address date_of_birth gender])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[phone_number first_name other_names address date_of_birth gender])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[phone_number])
+  end
 end
