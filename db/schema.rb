783e92cfaf9343025c6ebc8647accc121c0470e9
RoyNyaga <nyagaandreroy@gmail.com>
2023-12-16 23:54:24 +0100

    add phone_number to teachers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_16_224030) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_16_225108) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_16_224030) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number", null: false
     t.index ["email"], name: "index_teachers_on_email", unique: true
+    t.index ["phone_number"], name: "index_teachers_on_phone_number", unique: true
     t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
   end
 
