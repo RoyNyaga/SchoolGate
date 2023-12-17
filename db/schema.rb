a0731b1088944ced4121b357bf5579c893a49725
RoyNyaga <nyagaandreroy@gmail.com>
2023-12-17 15:04:06 +0100

    create collaboration model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_17_133749) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_17_135919) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,5 +38,18 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_17_133749) do
     t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
   end
 
+  create_table "workers", force: :cascade do |t|
+    t.bigint "teacher_id", null: false
+    t.bigint "school_id", null: false
+    t.integer "permission"
+    t.float "monthly_rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["school_id"], name: "index_workers_on_school_id"
+    t.index ["teacher_id"], name: "index_workers_on_teacher_id"
+  end
+
   add_foreign_key "schools", "teachers"
+  add_foreign_key "workers", "schools"
+  add_foreign_key "workers", "teachers"
 end
---
+++ test/system/workers_test.rb
@@ -0,0 +1,47 @@
+require "application_system_test_case"
+
+class WorkersTest < ApplicationSystemTestCase
+  setup do
+    @worker = workers(:one)
+  end
+
+  test "visiting the index" do
+    visit workers_url
+    assert_selector "h1", text: "Workers"
+  end
+
+  test "should create worker" do
+    visit workers_url
+    click_on "New worker"
+
+    fill_in "Monthly rate", with: @worker.monthly_rate
+    fill_in "Permission", with: @worker.permission
+    fill_in "School", with: @worker.school_id
+    fill_in "Teacher", with: @worker.teacher_id
+    click_on "Create Worker"
+
+    assert_text "Worker was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Worker" do
+    visit worker_url(@worker)
+    click_on "Edit this worker", match: :first
+
+    fill_in "Monthly rate", with: @worker.monthly_rate
+    fill_in "Permission", with: @worker.permission
+    fill_in "School", with: @worker.school_id
+    fill_in "Teacher", with: @worker.teacher_id
+    click_on "Update Worker"
+
+    assert_text "Worker was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Worker" do
+    visit worker_url(@worker)
+    click_on "Destroy this worker", match: :first
+
+    assert_text "Worker was successfully destroyed"
+  end
+end
