971654b193bee8f4b53129205e6113941c46d91b
RoyNyaga <nyagaandreroy@gmail.com>
2023-12-24 13:37:55 +0100

    add school_id validation

--- app/models/working.rb
+++ app/models/working.rb
@@ -1,4 +1,6 @@
 class Working < ApplicationRecord
   belongs_to :school
   belongs_to :teacher
+  validates :school_id, uniqueness: { scope: :teacher_id, 
+    message: "This teacher is already a worker to this school" }
 end
