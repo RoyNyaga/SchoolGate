98756d4c66bed9ebc490e55aa096777bbcc62462
RoyNyaga <nyagaandreroy@gmail.com>
2023-12-24 13:47:03 +0100

    set permission enum

--- app/models/working.rb
+++ app/models/working.rb
@@ -3,4 +3,7 @@ class Working < ApplicationRecord
   belongs_to :teacher
   validates :school_id, uniqueness: { scope: :teacher_id, 
     message: "This teacher is already a worker to this school" }
+
+  enum permission: { teacher: 0, descipline_master: 1, buster: 2, principal: 3 }
+
 end
