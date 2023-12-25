eb856b238333a8f449a075891c10cc4cabce0639
RoyNyaga <nyagaandreroy@gmail.com>
2023-12-25 23:24:47 +0100

    add uniqueness validation to teacher_id

--- app/models/teaching.rb
+++ app/models/teaching.rb
@@ -2,4 +2,7 @@ class Teaching < ApplicationRecord
   belongs_to :teacher
   belongs_to :subject
   belongs_to :school_class
+
+  validates :teacher_id, presence: true, uniqueness: { scope: :subject,
+                                                       message: ": Has already been assigned to this subject." }
 end
