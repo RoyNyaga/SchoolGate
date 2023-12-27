e2fa0ef4bdd34fc8322e5a555cb40dc11ea74a10
RoyNyaga <nyagaandreroy@gmail.com>
2023-12-28 00:25:32 +0100

    update hashed_marks

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -7,7 +7,11 @@ class Sequence < ApplicationRecord
   enum seq_num: { first_sequence: 1, second_sequence: 2, third_sequence: 3, forth_sequence: 4, fifth_sequence: 5, sith_sequence: 6 }
 
   def hashed_marks
-    self.marks.map { |m| eval(m) }
+    # parsing marks to ruby hash and converting the mark value to float
+    self.marks.map { |m| eval(m) }.map { |a|
+      { "id" => a["id"], "name" => a["name"], "mark" => a[
+        "mark"].to_f }
+    }
   end
 
   def title
