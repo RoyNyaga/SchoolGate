3d46deb636e40343f228d2f90c76d5a03cc06f8e
RoyNyaga <nyagaandreroy@gmail.com>
2023-12-31 11:40:26 +0100

    sum_sequence_subject_marks

--- app/models/term.rb
+++ app/models/term.rb
@@ -8,4 +8,35 @@ class Term < ApplicationRecord
   def title
     "#{term_type.humanize} #{academic_year_start} / #{academic_year_end}"
   end
+
+  def subject_rank_per_student(subject_id)
+    first_seq, second_seq = sequences.where(subject_id: subject.id).order(:seq_num)
+  end
+
+  def sum_sequence_subject_marks(first_seq, second_seq)
+    result = []
+    first_seq_marks = first_seq.hashed_marks
+    second_seq_marks = second_seq.hashed_marks
+
+    first_seq_marks.each do |first_seq_mark|
+      matching_hash = second_seq_marks.find { |second_seq_mark| second_seq_mark["id"] == first_seq_mark["id"] }
+
+      if matching_hash
+        sum_mark = first_seq_mark["mark"] + matching_hash["mark"]
+        result << { "id" => first_seq_mark["id"], "name" => first_seq_mark["name"], "mark" => sum_mark }
+      else
+        # Handle the case where there is no matching hash in second_seq_marks
+        result << first_seq_mark
+      end
+    end
+
+    # Add any remaining hashes from second_seq_marks that were not matched in first_seq_marks
+    second_seq_marks.each do |second_seq_mark|
+      unless result.any? { |result_hash| result_hash["id"] == second_seq_mark["id"] }
+        result << second_seq_mark
+      end
+    end
+
+    result
+  end
 end
