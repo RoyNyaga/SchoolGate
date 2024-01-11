963614642818ea658174228b02ab42e7253c5849
RoyNyaga <nyagaandreroy@gmail.com>
2024-01-11 15:06:30 +0100

    create TimeManipulation

---
+++ app/models/concerns/time_manipulation.rb
@@ -0,0 +1,17 @@
+# app/models/concerns/time_manipulation.rb
+module TimeManipulation
+  extend ActiveSupport::Concern
+
+  included do
+    def generate_academic_year
+      current_date = Date.today
+      start_year = current_date.month >= 8 ? current_date.year : current_date.year - 1
+      end_year = start_year + 1
+
+      "#{start_year}-#{end_year}"
+    end
+  end
+
+  class_methods do
+  end
+end
