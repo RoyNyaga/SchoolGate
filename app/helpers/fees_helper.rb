dd728f611241602ad8d9b5a1adb98c4b10820bb7
RoyNyaga <nyagaandreroy@gmail.com>
2024-01-15 22:48:15 +0100

    create fees_controller.js

--- app/helpers/fees_helper.rb
+++ app/helpers/fees_helper.rb
@@ -1,6 +1,7 @@
 module FeesHelper
   def generate_progress_bar(percent_complete)
-    content_tag(:div, "#{percent_complete} %", class: "progress-bar #{generate_progress_background(percent_complete)} text-dark fee-progress-fonts", role: "progressbar", "aria-label": "Example with label", style: "width: #{percent_complete}%", "aria-valuenow": percent_complete, "aria-valuemin": 0, "aria-valuemax": 100)
+    content_tag(:div, "#{percent_complete} %", class: "progress-bar #{generate_progress_background(percent_complete)} text-dark fee-progress-fonts",
+                                               role: "progressbar", "aria-label": "Example with label", style: "width: #{percent_complete}%", "aria-valuenow": percent_complete, "aria-valuemin": 0, "aria-valuemax": 100)
   end
 
   def generate_progress_background(percent_complete)
@@ -16,8 +17,12 @@ module FeesHelper
     end
   end
 
+  def total_fee_paid_span(percent_complete, total_feed_paid)
+    content_tag(:span, total_feed_paid, class: "badge #{generate_progress_background(percent_complete)}")
+  end
+
   def generate_installment_input_field(form, value)
-    form.number_field :installments, name: "fee[installments][]", value: value, class: "general-input-styles"
+    form.number_field :installments, name: "fee[installments][]", value: value, class: "general-input-styles", data: { action: "fees#calculateSum" }
   end
 
   def generate_student_marks_input(form, sequence, student)
--- app/javascript/controllers/fees_controller.js
+++ app/javascript/controllers/fees_controller.js
@@ -2,6 +2,28 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="fees"
 export default class extends Controller {
-  connect() {
+  addFeeInput() {
+    const lastInput = document.querySelector('#fee-installments-inputs input:last-child');
+  
+    // Create a new element from the last input
+    const newInput = lastInput.cloneNode(true);
+    newInput.value = ""; // reseting the value
+  
+    // Prepend the new element back to the "fee-installments-inputs"
+    const feeInstallmentsInputs = document.getElementById('fee-installments-inputs');
+    feeInstallmentsInputs.append(newInput);
+  }
+
+  calculateSum() {
+    const sumDiv = document.querySelector("#sumDiv")
+    // Get all input elements within the "fee-installments-inputs" div
+    const inputs = document.querySelectorAll('#fee-installments-inputs input');
+
+    // Calculate the sum of values
+    const sum = Array.from(inputs).reduce((total, input) => {
+      return total + parseFloat(input.value) || total;
+    }, 0);
+
+    sumDiv.textContent = `${sum} frs`
   }
 }
