eb7c347a01d902829865082df9cd784259271965
RoyNyaga <nyagaandreroy@gmail.com>
2024-01-30 03:24:59 +0100

    first upload implementation

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,7 @@
 class StudentsController < ApplicationController
   layout "school_layout"
   before_action :check_for_current_school
-  before_action :set_student, only: %i[ show edit update destroy ]
+  before_action :set_student, only: %i[ show edit update destroy update_photo ]
 
   # GET /students or /students.json
   def index
@@ -49,6 +49,22 @@ class StudentsController < ApplicationController
     end
   end
 
+  def update_photo
+    # # Convert the blob string to an IO object
+    # blob_io = StringIO.new(Base64.decode64(params[:student][:photo]))
+    # blob_io.class.class_eval { attr_accessor :original_filename, :content_type }
+    # blob_io.original_filename = "image.jpg" # Set a default filename
+    # blob_io.content_type = "image/jpeg" # Set the content type
+
+    # # Attach the blob to the Active Storage image_file
+    # @student.photo.attach(io: blob_io, filename: blob_io.original_filename, content_type: blob_io.content_type)
+    # @student.save
+    binding.break
+    # respond_to do |format|
+    #   format.json { render :edit, status: :ok, location: @student }
+    # end
+  end
+
   # DELETE /students/1 or /students/1.json
   def destroy
     @student.destroy!
@@ -70,6 +86,6 @@ class StudentsController < ApplicationController
   def student_params
     params.require(:student).permit(:school_id, :school_class_id, :fathers_name, :fathers_contact,
                                     :mothers_name, :mothers_contact, :guidance_name, :guidance_contact, :date_of_birth, :address, :subjects,
-                                    :town, :first_name, :last_name)
+                                    :town, :first_name, :last_name, :photo)
   end
 end
