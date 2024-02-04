a7845eccb4d855bd0a5036e6d4e49ed509821631
RoyNyaga <nyagaandreroy@gmail.com>
2024-02-04 16:28:23 +0100

    add current_school_classes_link_data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_school
+  helper_method :current_school, :current_school_classes_link_data
   protect_from_forgery with: :exception
   before_action :authenticate_teacher!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def current_school
     @current_school ||= School.find(session[:current_school_id]) if session[:current_school_id]
+  end
+
+  def current_school_classes_link_data
+    @current_school.school_classes.map do |school_class|
+      { class_name: school_class.name, class_path: school_class_path(school_class.id) }
+    end
   end
 
   def check_for_current_school
