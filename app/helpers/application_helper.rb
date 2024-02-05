0901c622e55a58dd27e02745a40af5372cde54b1
RoyNyaga <nyagaandreroy@gmail.com>
2024-02-06 00:56:59 +0100

    more data links to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,29 @@ module ApplicationHelper
   def photo_action_name(record)
     record.photo.attached? ? "Change Photo" : "Upload Photo"
   end
+
+  def generate_sidebar_expanded_div_id(link_name)
+    "sidebar_classes_expand_#{link_name.gsub(" ", "-").downcase}"
+  end
+
+  def current_school_classes_link_data
+    current_school.school_classes.map do |school_class|
+      { name: school_class.name, path: school_class_path(school_class.id) }
+    end
+  end
+
+  def current_school_teachers_link_data
+    [
+      { name: "Invitations", path: invitations_schools_path },
+      { name: "Contracts", path: contracts_schools_path },
+      { name: "Permissions", path: permissions_schools_path },
+    ]
+  end
+
+  def current_school_report_cards_link_data
+    [
+      { name: "Auto Generate (bulk)", path: auto_generate_report_cards_path },
+      { name: "Manually Create", path: manually_create_report_cards_path },
+    ]
+  end
 end
