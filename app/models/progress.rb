787c2b8da2d5b88907fa6752c4228a168b6ff2a3
RoyNyaga <nyagaandreroy@gmail.com>
2024-02-18 22:56:29 +0100

    reference school_class_id in related models

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -3,4 +3,7 @@ class Progress < ApplicationRecord
   belongs_to :subject
   belongs_to :teacher
   belongs_to :term
+  belongs_to :school_class
+
+  enum seq_num: { first_sequence: 1, second_sequence: 2, third_sequence: 3, forth_sequence: 4, fifth_sequence: 5, sith_sequence: 6 }
 end
