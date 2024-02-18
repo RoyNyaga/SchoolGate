c529cb98332b3c0f5c0bc4623f56c25225f219ba
RoyNyaga <nyagaandreroy@gmail.com>
2024-02-18 23:18:39 +0100

    prevent adding of multiple topics

--- app/javascript/controllers/progresses_controller.js
+++ app/javascript/controllers/progresses_controller.js
@@ -6,23 +6,28 @@ export default class extends Controller {
   }
 
   addTopicToForm = (e) => {
+    const addedTopics = Array.from(document.querySelectorAll('.added-topic'));
+    const topicIds = addedTopics.map(student => student.getAttribute('data-topicid'));
     const title = e.target.getAttribute("data-topic-title")
     const id = e.target.getAttribute("data-topic-id")
-    const topicToBeAdded = this.generateTopicFormElement(title, id)
-    // Creating a temporary div to hold the generated HTML
-    const tempDiv = document.createElement('div');
-    tempDiv.innerHTML = topicToBeAdded;
-    // Finding the first element node
-    var generatedElement;
-    for (var i = 0; i < tempDiv.childNodes.length; i++) {
-      if (tempDiv.childNodes[i].nodeType === 1) { // 1 represents an element node
-        generatedElement = tempDiv.childNodes[i];
-        break;
+    if (!topicIds.includes(id)) {
+      const topicToBeAdded = this.generateTopicFormElement(title, id)
+      // Creating a temporary div to hold the generated HTML
+      const tempDiv = document.createElement('div');
+      tempDiv.innerHTML = topicToBeAdded;
+      // Finding the first element node
+      var generatedElement;
+      for (var i = 0; i < tempDiv.childNodes.length; i++) {
+        if (tempDiv.childNodes[i].nodeType === 1) { // 1 represents an element node
+          generatedElement = tempDiv.childNodes[i];
+          break;
+        }
       }
-    }
-    const parentDiv = document.querySelector(".added-topics-wrapper")
+      const parentDiv = document.querySelector(".added-topics-wrapper")
 
-    parentDiv.appendChild(generatedElement);
+      parentDiv.appendChild(generatedElement);
+    }
+
   }
 
   removeTopic = (e) => {
@@ -33,7 +38,7 @@ export default class extends Controller {
 
   generateTopicFormElement = (topicTitle, topicId) => {
     return `
-    <div class="added-topic" id="added_topic_${topicId}">
+    <div class="added-topic" id="added_topic_${topicId}" data-topicid=${topicId}>
           <h6 class="added-topic-header">${topicTitle}</h6>
           <div class="added-topic-body">
             <input name="progress[topics][][id]" value=${topicId} autocomplete="off" type="hidden" id="progress_id">
