0acd32eece5ccf124bac6a1dadf55d289964cc45
RoyNyaga <nyagaandreroy@gmail.com>
2024-02-19 03:10:57 +0100

    add percent_complete to main_topics table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_19_012850) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_19_021004) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -114,6 +114,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_19_012850) do
     t.boolean "is_complete", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "percent_complete", default: 0.0
     t.index ["curriculum_id"], name: "index_main_topics_on_curriculum_id"
     t.index ["subject_id"], name: "index_main_topics_on_subject_id"
     t.index ["teacher_id"], name: "index_main_topics_on_teacher_id"
