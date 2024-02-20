20eb72d75c42eb765b5d56d01d61a3f6feed2ca9
RoyNyaga <nyagaandreroy@gmail.com>
2024-02-21 00:10:39 +0100

    update curriculum

--- app/models/curriculum.rb
+++ app/models/curriculum.rb
@@ -8,4 +8,18 @@ class Curriculum < ApplicationRecord
   has_many :main_topics
 
   validates :title, presence: true
+
+  def self.update_completion_state(main_topic_ids)
+    curriculums = Curriculum.joins(:main_topics).where(main_topics: { id: main_topic_ids }).distinct
+    curriculums.each do |curriculum|
+      curriculum_maintopic_group_data = curriculum.main_topics.group(:is_complete).count
+      total_main_topic = curriculum_maintopic_group_data.values.sum
+      if curriculum_maintopic_group_data[true].present? && curriculum_maintopic_group_data[true] > 0
+        curriculum_percent_complete = (curriculum_maintopic_group_data[true] / total_main_topic).to_f * 100
+
+        curriculum.update(is_complete: curriculum_percent_complete >= 100,
+                          percent_complete: curriculum_percent_complete)
+      end
+    end
+  end
 end
