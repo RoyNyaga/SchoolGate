8a6d4495002c8b21f15e8d3dcb3992ccb9de0083
RoyNyaga <nyagaandreroy@gmail.com>
2024-02-21 12:15:46 +0100

    update teachers_controller

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,6 +1,7 @@
 class TeachersController < ApplicationController
   include ApplicationHelper # this is to enable us access the generate_modal_id helper method from this controller
   before_action :set_teacher, only: [:edit, :update_photo]
+  before_action :check_for_current_school, only: [:subjects, :progresses]
 
   def invitations
     @pending = current_teacher.invitations.pending
@@ -29,6 +30,9 @@ class TeachersController < ApplicationController
   def progresses
     @progresses = current_teacher.progresses
     @total_hours_mins_time = Progress.calc_total_time(@progresses)
+    @topics_covered = @progresses.map { |p| p.topics.count }.sum
+    @absentist_num = @progresses.map { |p| p.absent_students.count }.sum
+
     render layout: "school_layout"
   end
 
