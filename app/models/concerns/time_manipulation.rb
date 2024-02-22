81e98e43e78513ba82393924155b20c82a8c1724
RoyNyaga <nyagaandreroy@gmail.com>
2024-02-22 22:43:48 +0100

    update time manipulation

--- app/models/concerns/time_manipulation.rb
+++ app/models/concerns/time_manipulation.rb
@@ -3,6 +3,11 @@ module TimeManipulation
   extend ActiveSupport::Concern
 
   included do
+    def readable_date(date: nil, with_year: false, with_time: false)
+      format = with_year ? "%a %b %d, %Y" : "%a %b %d"
+      date ||= created_at
+      readable_date = with_time ? "#{date.strftime(format)} - #{date.strftime("%l:%M%P").strip.downcase}" : date.strftime(format)
+    end
   end
 
   class_methods do
@@ -12,6 +17,12 @@ module TimeManipulation
       end_year = start_year + 1
 
       "#{start_year}/#{end_year}"
+    end
+
+    def generate_start_and_end_week_dates(date)
+      start_of_week = date.beginning_of_week
+      end_of_week = date.end_of_week
+      { start: start_of_week, end: end_of_week }
     end
   end
 end
