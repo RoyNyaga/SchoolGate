6b16a136be86f236e566ec0a824e141f1bbc549a
RoyNyaga <nyagaandreroy@gmail.com>
2024-02-24 03:00:46 +0100

    update

--- app/helpers/sequences_helper.rb
+++ app/helpers/sequences_helper.rb
@@ -7,6 +7,17 @@ module SequencesHelper
     end
   end
 
+  def generate_is_enrolled_input(form, sequence, student)
+    mark_object = student.get_mark_object(sequence.hashed_marks)
+    is_enrolled = mark_object.present? ? mark_object["is_enrolled"] : false
+    options = [["Yes", true], ["No", false]]
+    selected_option = is_enrolled ? options.first : options.last
+    options.delete_if { |item| item == selected_option } # remove selected_option from options
+    selected_option[2] = { selected: true } # update selected_option to include { selected: true } in it's array
+    options << selected_option # add the updated selected_options back to options
+    form.select :marks, options, {}, { name: "sequence[marks][][is_enrolled]" }
+  end
+
   def generate_years
     [Date.today.year + 1, Date.today.year, Date.today.year - 1]
   end
