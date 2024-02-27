2d2ad42e7a2ef078a73bd191fa6785d00163d0f3
RoyNyaga <nyagaandreroy@gmail.com>
2024-02-27 11:09:43 +0100

    update models/topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,19 +5,18 @@ class Topic < ApplicationRecord
 
   enum progress: { to_do: 1, in_progress: 2, completed: 3 }
 
-  after_update :update_main_topic
+  after_create :update_main_topic
 
   # private
 
   def update_main_topic
-    binding.break
-
     topic_progress_group_data = main_topic.topics.group(:progress).count
     total_topic_num = topic_progress_group_data.values.sum
     if topic_progress_group_data["completed"] > 0
-      main_topic_percent_complete = (topic_progress_group_data[:completed] / total_topic_num).to_f * 100
+      main_topic_percent_complete = (topic_progress_group_data["completed"].to_f / total_topic_num) * 100
       main_topic.update(percent_complete: main_topic_percent_complete,
                         is_complete: main_topic_percent_complete >= 100)
+      # binding.break
     end
   end
 end
