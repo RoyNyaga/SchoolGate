ddf711a073ea3466a086c2b06944cbaf69119a67
RoyNyaga <nyagaandreroy@gmail.com>
2024-02-27 23:29:16 +0100

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_213745) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_27_222637) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -179,6 +179,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_22_213745) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.json "school_fees_settings", default: {}
+    t.jsonb "student_id_settings"
     t.index ["teacher_id"], name: "index_schools_on_teacher_id"
   end
 
