328b73ba626d9cd717f060170cd57a9076bf2ab6
RoyNyaga <nyagaandreroy@gmail.com>
2024-02-27 11:05:23 +0100

    update progresses_helper

--- app/helpers/progresses_helper.rb
+++ app/helpers/progresses_helper.rb
@@ -1,2 +1,10 @@
 module ProgressesHelper
+  def topic_progress_select(form, topic)
+    options = [["in_progress", "2"], ["completed", "3"]]
+    options.each do |option|
+      option << { selected: true } if option[0] == topic.progress
+    end
+
+    form.select :marks, options, {}, { name: "progress[topics][][progress]", class: "progress-curriculum-select-input" }
+  end
 end
