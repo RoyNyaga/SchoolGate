f4842889f092d38a808123dd882f95593d2a9158
RoyNyaga <nyagaandreroy@gmail.com>
2024-02-27 11:05:04 +0100

    add progresses_controller

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -1,7 +1,7 @@
 class ProgressesController < ApplicationController
   layout "school_layout"
   before_action :check_for_current_school
-  before_action :set_progress, only: %i[ show edit update destroy ]
+  before_action :set_progress, only: %i[ show edit update destroy more_details ]
 
   # GET /progresses or /progresses.json
   def index
@@ -22,6 +22,12 @@ class ProgressesController < ApplicationController
 
   # GET /progresses/1/edit
   def edit
+    @subject = @progress.subject
+    @curriculums = Curriculum.where(subject_id: @subject.id, teacher_id: current_teacher.id,
+                                    academic_year: Curriculum.generate_current_academic_year)
+
+    topic_ids = Progress.string_to_hash_arr(@progress.topics).map { |t| t["id"] }
+    @topics = Topic.where(id: topic_ids)
   end
 
   # POST /progresses or /progresses.json
@@ -32,7 +38,7 @@ class ProgressesController < ApplicationController
       if @progress.save
         topic_ids = Progress.string_to_hash_arr(@progress.topics).map { |t| t["id"].to_i }
         Progress.update_curriculum(topic_ids)
-        format.html { redirect_to progress_url(@progress), notice: "Progress was successfully created." }
+        format.html { redirect_to for_teacher_subject_path(@progress.subject_id), notice: "Progress was successfully created." }
         format.json { render :show, status: :created, location: @progress }
       else
         @subject = Subject.find(@progress.subject_id)
@@ -48,13 +54,22 @@ class ProgressesController < ApplicationController
   def update
     respond_to do |format|
       if @progress.update(progress_params)
-        format.html { redirect_to progress_url(@progress), notice: "Progress was successfully updated." }
+        topic_ids = Progress.string_to_hash_arr(@progress.topics).map { |t| t["id"].to_i }
+        Progress.update_curriculum(topic_ids)
+        format.html { redirect_to progresses_teacher_path(current_teacher), success: "Progress was successfully updated." }
         format.json { render :show, status: :ok, location: @progress }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @progress.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def more_details
+    topic_ids = Progress.string_to_hash_arr(@progress.topics).map { |t| t["id"] }
+    @topics = Topic.where(id: topic_ids)
+    absent_student_ids = Progress.string_to_hash_arr(@progress.absent_students).map { |t| t["id"] }
+    @absent_students = Student.where(id: absent_student_ids)
   end
 
   # DELETE /progresses/1 or /progresses/1.json
@@ -76,6 +91,7 @@ class ProgressesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def progress_params
+    params[:progress][:absent_students] = [] if params[:progress][:absent_students].nil?
     params.require(:progress).permit(:school_id, :subject_id, :teacher_id, :duration, :term_id, :academic_year,
                                      :seq_num, :hours, :mins, :school_class_id, topics: [:id, :progress], absent_students: [:id, :full_name])
   end
