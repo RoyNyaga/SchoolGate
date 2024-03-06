d007fbc27f3f423ea546666b22bdfcdd3bbb58a6
RoyNyaga <nyagaandreroy@gmail.com>
2024-03-06 23:09:26 +0100

    update models/report_card

--- app/models/report_card.rb
+++ app/models/report_card.rb
@@ -1,4 +1,5 @@
 class ReportCard < ApplicationRecord
+  include DataTrans
   belongs_to :school
   belongs_to :school_class
   belongs_to :term
@@ -67,5 +68,29 @@ class ReportCard < ApplicationRecord
   def self.add_class_average
     average = @bulk_report.map { |r| r[:average] }.sum / @bulk_report.size
     @bulk_report.each { |r| r[:class_average] = average }
+  end
+
+  def generate_chart_data
+    arr = ReportCard.string_to_hash_arr(details)
+    main_data = [
+      { "name" => "first_seq_mark", "data" => {} },
+      { "name" => "second_seq_mark", "data" => {} },
+    ]
+    arr.each do |subject, index|
+      seq_obj = {
+        "first_seq_mark_data" => { "#{subject[:name][0..3]}" => subject[:first_seq_mark] },
+        "second_seq_mark_data" => { "#{subject[:name][0..3]}" => subject[:second_seq_mark] },
+      }
+
+      main_data.each_with_index do |obj, index|
+        current_key = index == 0 ? "first_seq_mark_data" : "second_seq_mark_data"
+        obj["data"] = obj["data"].merge(seq_obj[current_key])
+      end
+    end
+    main_data
+  end
+
+  def long_title
+    "#{term.term_type} #{term.academic_year} #{school_class.name}"
   end
 end
