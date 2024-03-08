8a43b7b45a1b68a56417f02a19ac52b2c4c4b79f
RoyNyaga <nyagaandreroy@gmail.com>
2024-03-08 23:14:47 +0100

    create reportCards_controller

---
+++ app/javascript/controllers/reportCards_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="photo-uploads"
+export default class extends Controller {
+
+  connect() {
+    console.log("Report cards controller loaded")
+  }
+
+
+}
