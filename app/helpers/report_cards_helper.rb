ea488bf4839173238a1751714e66fe7a8077fac5
RoyNyaga <nyagaandreroy@gmail.com>
2024-03-08 23:13:22 +0100

    update report_cards_heler

--- app/helpers/report_cards_helper.rb
+++ app/helpers/report_cards_helper.rb
@@ -1,7 +1,6 @@
 module ReportCardsHelper
-  def detail_dynamic_inputs(form, input_type, input_name, value, class_names: "inputs")
+  def detail_dynamic_inputs(form, input_type, input_name, value, class_names: "inputs", disabled: false)
     # form.number_field :mark, name: "sequence[marks][][mark]", step: "any", class: "sequence-mark-input"
-
-    form.send(input_type, :details, name: input_name, value: value, class: class_names)
+    form.send(input_type, :details, name: input_name, value: value, class: class_names, disabled: disabled)
   end
 end
