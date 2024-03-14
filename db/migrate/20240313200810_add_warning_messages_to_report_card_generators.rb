65d593b8cb7bc9a8af4a24be4ef1735c334d92f1
RoyNyaga <nyagaandreroy@gmail.com>
2024-03-14 19:29:46 +0100

    add least_performed_students to report_card_generators table

--- db/migrate/20240313200810_add_warning_messages_to_report_card_generators.rb
+++ db/migrate/20240313200810_add_warning_messages_to_report_card_generators.rb
@@ -1,5 +1,7 @@
 class AddWarningMessagesToReportCardGenerators < ActiveRecord::Migration[7.1]
   def change
     add_column :report_card_generators, :warning_messages, :text, array: true, default: []
+    add_column :report_card_generators, :process_duration, :float, default: 0.0
+    add_column :report_card_generators, :least_performed_students, :text, array: true, default: []
   end
 end
