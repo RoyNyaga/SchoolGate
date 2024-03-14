108b365aad474a8313e8655d4b46469073b72060
RoyNyaga <nyagaandreroy@gmail.com>
2024-03-14 19:32:34 +0100

    update app/models/subject.rb

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -6,7 +6,27 @@ class Subject < ApplicationRecord
   has_many :sequences, dependent: :destroy
   has_many :topics, dependent: :destroy
 
+  store_accessor :remarks, :less_than_equal_to_5, :less_than_equal_to_9, :less_than_equal_to_12,
+                 :less_than_equal_to_15, :less_than_equal_to_18, :less_than_equal_to_20
+
   def total_on_marks
     20 * coefficient
+  end
+
+  def add_remarks(average)
+    case average
+    when average <= 5
+      less_than_equal_to_5
+    when average <= 9
+      less_than_equal_to_9
+    when average <= 12
+      less_than_equal_to_12
+    when average <= less_than_equal_to_15
+      less_than_equal_to_15
+    when average <= less_than_equal_to_18
+      less_than_equal_to_18
+    when average <= less_than_equal_to_20
+      less_than_equal_to_20
+    end
   end
 end
