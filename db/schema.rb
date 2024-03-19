6bb98f543be89c06a7e44586d973b36d4f8ee556
RoyNyaga <nyagaandreroy@gmail.com>
2024-03-20 00:40:50 +0100

    add report_card_generator to report_cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_17_125707) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_19_151744) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -187,7 +187,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_17_125707) do
     t.float "total_score", default: 0.0
     t.float "total_coefficient", default: 0.0
     t.bigint "academic_year_id", null: false
+    t.bigint "report_card_generator_id", default: 3, null: false
     t.index ["academic_year_id"], name: "index_report_cards_on_academic_year_id"
+    t.index ["report_card_generator_id"], name: "index_report_cards_on_report_card_generator_id"
     t.index ["school_class_id"], name: "index_report_cards_on_school_class_id"
     t.index ["school_id"], name: "index_report_cards_on_school_id"
     t.index ["student_id"], name: "index_report_cards_on_student_id"
@@ -369,6 +371,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_17_125707) do
   add_foreign_key "report_card_generators", "schools"
   add_foreign_key "report_card_generators", "terms"
   add_foreign_key "report_cards", "academic_years"
+  add_foreign_key "report_cards", "report_card_generators"
   add_foreign_key "report_cards", "school_classes"
   add_foreign_key "report_cards", "schools"
   add_foreign_key "report_cards", "students"
