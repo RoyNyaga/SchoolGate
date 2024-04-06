b4a673bd0ad46ace772fb8e374ecab0c5a9f9ba7
RoyNyaga <nyagaandreroy@gmail.com>
2024-04-06 01:49:18 +0100

    create department_show_card

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,4 +1,5 @@
 class DepartmentsController < ApplicationController
+  layout "school_layout"
   before_action :set_department, only: %i[ show edit update destroy ]
 
   # GET /departments or /departments.json
