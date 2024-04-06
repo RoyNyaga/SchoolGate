979b6e33a9f361e4d0f3100f2bd5da91e545dec5
RoyNyaga <nyagaandreroy@gmail.com>
2024-04-06 02:16:27 +0100

    add validation to courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,7 @@ class Course < ApplicationRecord
   belongs_to :school
   belongs_to :faculty
   belongs_to :department
+
+  validates :name, presence: true
+  validates :credit_value, presence: true
 end
