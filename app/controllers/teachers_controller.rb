85a832b72916d0fa1d828d2d8ff409a3ceee2918
RoyNyaga <nyagaandreroy@gmail.com>
2024-04-09 00:57:23 +0100

    app/controllers/teachers_controller.rb

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -7,6 +7,7 @@ class TeachersController < ApplicationController
     @pending = current_teacher.invitations.pending
     @accepted = current_teacher.invitations.accepted
     @rejected = current_teacher.invitations.rejected
+    render layout: "school_layout"
   end
 
   def subjects
