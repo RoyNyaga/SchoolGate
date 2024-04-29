f7d9cbc6f55855b43be8088a37e980e97011fa3d
RoyNyaga <nyagaandreroy@gmail.com>
2024-04-29 13:41:42 +0100

    update time_manipulation

--- app/models/concerns/time_manipulation.rb
+++ app/models/concerns/time_manipulation.rb
@@ -11,14 +11,6 @@ module TimeManipulation
   end
 
   class_methods do
-    def generate_current_academic_year
-      current_date = Date.today
-      start_year = current_date.month >= 8 ? current_date.year : current_date.year - 1
-      end_year = start_year + 1
-
-      "#{start_year}/#{end_year}"
-    end
-
     def generate_start_and_end_week_dates(date)
       start_of_week = date.beginning_of_week
       end_of_week = date.end_of_week
