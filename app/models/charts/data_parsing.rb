1ffc5902271c5d5044d0b434418fabcc2510122f
RoyNyaga <nyagaandreroy@gmail.com>
2024-04-29 13:42:01 +0100

    update data_parsing

--- app/models/charts/data_parsing.rb
+++ app/models/charts/data_parsing.rb
@@ -1,10 +1,10 @@
 class Charts::DataParsing
-  def self.revenue_per_class_over_time(school_classes, academic_year)
+  def self.revenue_per_class_over_time(school_classes, academic_year_text)
     main_data = []
     school_classes.each do |s_c|
       class_data = {}
       class_data["name"] = s_c.name
-      update_records = s_c.fees.where(academic_year: academic_year).map(&:update_records).flatten
+      update_records = s_c.fees.where(academic_year_text: academic_year_text).map(&:update_records).flatten
       update_records = update_records.map { |r| eval(r) }
       data = {}
       update_records.each do |record|
@@ -16,11 +16,11 @@ class Charts::DataParsing
     main_data
   end
 
-  def self.completeness_fee_per_class(academic_year, complete_type, current_school)
+  def self.completeness_fee_per_class(academic_year_text, complete_type, current_school)
     data = {}
     school_classes = current_school.school_classes.includes(:fees)
     school_classes.each do |s_c|
-      data["#{s_c.name}"] = s_c.fees.send(complete_type).where(academic_year: academic_year).count # fees should be in the context of the academic year selected
+      data["#{s_c.name}"] = s_c.fees.send(complete_type).where(academic_year_text: academic_year_text).count # fees should be in the context of the academic year selected
     end
     data
   end
