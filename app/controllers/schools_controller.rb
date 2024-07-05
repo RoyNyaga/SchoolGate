fb938a8ada70cf4ccbbf90078f51bd06139eabde
RoyNyaga <nyagaandreroy@gmail.com>
2024-07-05 02:11:14 +0100

    app/controllers/schools_controller.rb

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -120,7 +120,7 @@ class SchoolsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def school_params
-    params.require(:school).permit(:full_name, :abbreviation, :town, :address, :moto, :level_1_fees, :level_2_fees,
+    params.require(:school).permit(:full_name, :abbreviation, :town, :address, :moto, :notification_contact, :activate_notification, :level_1_fees, :level_2_fees,
                                    :level_3_fees, :level_4_fees, :level_5_fees, :level_6_fees, :level_7_fees, :school_identifier, :education_level)
   end
 end
