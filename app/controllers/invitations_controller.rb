6488594d7e6f7de0ececab57691d18688e5bf403
RoyNyaga <nyagaandreroy@gmail.com>
2024-07-08 00:27:35 +0100

    app/controllers/invitations_controller.rb

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -3,22 +3,25 @@ class InvitationsController < ApplicationController
   before_action :check_for_current_school, only: [:create]
 
   def new
+    @invitation = Invitation.new
+    render layout: "school_layout"
   end
 
   # POST /invitations or /invitations.json
   def create
     @invitation = Invitation.new(invitation_params)
-    if current_school.workers.where(teacher_id: params[:teacher_id]).present?
+    binding.break
+    @teacher = Teacher.find_by(phone_number: params[:invitation][:teachers_phone_number])
+    if @teacher && current_school.workers.where(id: @teacher.id).present?
       @status = "accepted"
       flash[:warning] = "This teacher is already a worker in this school"
       redirect_to invitations_schools_path(status: @status)
     else
       respond_to do |format|
         if @invitation.save
-          format.html { redirect_to request.referer, notice: "invitation was successfully created." }
+          format.html { redirect_to invitations_schools_path(status: "pending"), success: "invitation was successfully created." }
         else
           format.html { render :new, status: :unprocessable_entity }
-          format.json { render json: @invitation.errors, status: :unprocessable_entity }
         end
       end
     end
@@ -54,6 +57,6 @@ class InvitationsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def invitation_params
-    params.require(:invitation).permit(:school_id, :teacher_id, :sender_id, :permission, :proposed_salary, :job_description)
+    params.require(:invitation).permit(:school_id, :teacher_id, :sender_id, :permission, :proposed_salary, :job_description, :teachers_phone_number)
   end
 end
