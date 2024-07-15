5416ac4c002e1a8ad0ff9b2c43ca871e072a4fb7
RoyNyaga <nyagaandreroy@gmail.com>
2024-07-15 17:16:24 +0100

    app/controllers/schools_controller.rb

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -31,7 +31,7 @@ class SchoolsController < ApplicationController
 
     respond_to do |format|
       if @school.save
-        format.html { redirect_to school_url(@school), notice: "School was successfully created." }
+        format.html { redirect_to edit_school_path(@school), notice: "School was successfully created." }
         format.json { render :show, status: :created, location: @school }
       else
         format.html { render :new, status: :unprocessable_entity }
