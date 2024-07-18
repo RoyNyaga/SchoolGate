1687695c2048d66ba800ffaeb2c50b13a3aae612
RoyNyaga <nyagaandreroy@gmail.com>
2024-07-18 14:44:56 +0100

    app/models/working.rb

--- app/models/working.rb
+++ app/models/working.rb
@@ -1,10 +1,11 @@
 class Working < ApplicationRecord
   belongs_to :school
   belongs_to :teacher
-  validates :school_id, uniqueness: { scope: :teacher_id, 
-    message: "This teacher is already a worker to this school" }
+  validates :school_id, uniqueness: { scope: :teacher_id,
+                                      message: "This teacher is already a worker in this school" }
 
   enum permission: { teacher: 0, descipline_master: 1, buster: 2, principal: 3 }
-  enum status: { active: 0, inactive: 1 }
+  enum status: { active: 0, suspend: 1 }
 
+  validates :permission, presence: true
 end
