8dbf19407aebc20c83b9f27948660cbb3d140393
RoyNyaga <nyagaandreroy@gmail.com>
2024-07-18 23:53:59 +0100

    db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_08_235928) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_18_142712) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -238,6 +238,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_08_235928) do
     t.index ["school_class_id"], name: "index_fees_on_school_class_id"
     t.index ["school_id"], name: "index_fees_on_school_id"
     t.index ["student_id"], name: "index_fees_on_student_id"
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
   create_table "invitations", force: :cascade do |t|
@@ -656,6 +667,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_08_235928) do
     t.index ["main_topic_id"], name: "index_topics_on_main_topic_id"
     t.index ["subject_id"], name: "index_topics_on_subject_id"
     t.index ["teacher_id"], name: "index_topics_on_teacher_id"
+  end
+
+  create_table "tutorials", force: :cascade do |t|
+    t.string "youtube_frame"
+    t.string "slug"
+    t.string "title"
+    t.integer "views"
+    t.integer "priority"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "workings", force: :cascade do |t|
