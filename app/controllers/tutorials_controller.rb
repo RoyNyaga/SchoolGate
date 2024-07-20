577f520cd61fc7348b379917647c915add1537a9
RoyNyaga <nyagaandreroy@gmail.com>
2024-07-20 21:54:16 +0100

    app/controllers/tutorials_controller.rb

---
+++ app/controllers/tutorials_controller.rb
@@ -0,0 +1,46 @@
+class TutorialsController < InheritedResources::Base
+  before_action :set_tutorial, only: [:show, :edit, :update]
+
+  def index
+    @tutorials = Tutorial.all.order(priority: :asc)
+    @tutorial = @tutorials.first
+  end
+
+  def show
+    @tutorials = Tutorial.all.order(priority: :asc)
+  end
+
+  def new
+    @tutorial = Tutorial.new
+  end
+
+  def create
+    @tutorial = Tutorial.new(tutorial_params)
+    if @tutorial.save
+      redirect_to @tutorial
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @tutorial.update(tutorial_params)
+      redirect_to @tutorial
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_tutorial
+    @tutorial = Tutorial.friendly.find(params[:id])
+  end
+
+  def tutorial_params
+    params.require(:tutorial).permit(:youtube_frame, :slug, :title, :priority)
+  end
+end
