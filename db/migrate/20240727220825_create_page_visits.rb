bb63ec2bb0c06c3ac0b3dcf2a816f406dd833b53
RoyNyaga <nyagaandreroy@gmail.com>
2024-07-28 03:51:03 +0100

    db/migrate/20240727220825_create_page_visits.rb

--- db/migrate/20240727220825_create_page_visits.rb
+++ db/migrate/20240727220825_create_page_visits.rb
@@ -2,7 +2,7 @@ class CreatePageVisits < ActiveRecord::Migration[7.1]
   def change
     create_table :page_visits do |t|
       t.json :pages, default: {}
-
+      t.integer :total_visit_count, default: 0
       t.timestamps
     end
   end
