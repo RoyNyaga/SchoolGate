1540e4795cd9e22976d5c163026a96fc513b8074
RoyNyaga <nyagaandreroy@gmail.com>
2024-07-29 06:32:46 +0100

    ad messages to active admin dashboard

---
+++ app/admin/messages.rb
@@ -0,0 +1,9 @@
+ActiveAdmin.register Message do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :email, :phone_number, :phone_number_code, :country, :message
+end
