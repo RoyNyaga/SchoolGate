4ea381d9a2b7d648407a091d585658fbabda1cee
RoyNyaga <nyagaandreroy@gmail.com>
2024-11-10 18:20:15 +0100

    app/helpers/students_helper.rb

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -2,9 +2,9 @@ module StudentsHelper
   def student_status_color_badge(student)
     if student.status == "active"
       "bg-primary"
-    elsif sequence.status == "dropout"
+    elsif student.status == "dropout"
       "bg-warning"
-    elsif sequence.status == "dismissed"
+    elsif student.status == "dismissed"
       "bg-danger"
     end
   end
