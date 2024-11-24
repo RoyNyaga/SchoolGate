dc4b817837e69efd0588b0b958714d9d130bfdb5
RoyNyaga <nyagaandreroy@gmail.com>
2024-11-24 01:33:36 +0100

    app/controllers/performance_sheets_controller.rb

--- app/controllers/performance_sheets_controller.rb
+++ app/controllers/performance_sheets_controller.rb
@@ -1,9 +1,14 @@
-class PerformanceSheetsController < InheritedResources::Base
+class PerformanceSheetsController < ApplicationController
+  def create
+  end
 
   private
 
-    def performance_sheet_params
-      params.require(:performance_sheet).permit(:school_id, :academic_year_id, :teacher_id, :school_class_id, :term_id, :seq_num, :processing_time)
-    end
+  def set_performance_sheet
+    @performance_sheet = PerformanceSheet.find_by(id: params[:id])
+  end
 
+  def performance_sheet_params
+    params.require(:performance_sheet).permit(:school_id, :academic_year_id, :teacher_id, :school_class_id, :term_id, :seq_num, :processing_time)
+  end
 end
