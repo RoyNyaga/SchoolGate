de8f1ce44ab262db36ece0da757b6d377d27b741
RoyNyaga <nyagaandreroy@gmail.com>
2024-12-12 12:26:52 +0100

    app/models/report_card_generator.rb

--- app/models/report_card_generator.rb
+++ app/models/report_card_generator.rb
@@ -133,7 +133,7 @@ class ReportCardGenerator < ApplicationRecord
       @report_card_generator.report_card_file.attach(io: pdf_path, filename: file_name)
       pdf_generator.delete_pdf_file
       process_duration = Time.now - start_time
-      @report_card_generator.update(progress_state: 4, process_duration: process_duration)
+      @report_card_generator.update(progress_state: 4, process_duration: process_duration, is_processing: false)
     end
   end
 
@@ -151,76 +151,73 @@ class ReportCardGenerator < ApplicationRecord
     @has_unapproved_sequence = false
     @warning_messages = []
     @enrollment = @school_class.students.active.size
-    check_sequences_status
-    unless @has_unapproved_sequence
-      @students.each do |student|
-        next unless student.active?
-        total_score = 0
-        total_coefficient = 0
-        report_card_object = { school_id: @school.id, school_class_id: @school_class.id, term_id: @term.id,
-                               student_id: student.id, academic_year_id: @academic_year.id, report_card_generator_id: @report_card_generator.id }
-        details = []
-        passed_subjects = 0
-        @subjects.each do |subject|
-          subject_detail = {}
-          first_seq, second_seq = subject.sequences.approved.where(term_id: @term.id, academic_year_id: @academic_year.id).order(:seq_num)
-          if first_seq.present? && second_seq.present?
-            if student.was_enrolled?(first_seq.hashed_marks) && student.was_enrolled?(second_seq.hashed_marks)
-              sequence_marks = @term.sum_sequence_subject_marks(subject.id)
-              sequence_averages = @term.calc_sequence_averages(sequence_marks)
-              first_seq_mark = student.sequence_mark_per_subject(first_seq.hashed_marks)
-              second_seq_mark = student.sequence_mark_per_subject(second_seq.hashed_marks)
-              total_coefficient += subject.coefficient
-              average_mark = (first_seq_mark + second_seq_mark) / 2
-              score = average_mark * subject.coefficient
-              total_score += score
-              passed_subjects += 1 if average_mark >= 10 # assuming that we are working on 20. In the future, passed mark could be a setting at the level of the school and class
-              subject_detail["name"] = subject.name
-              subject_detail["id"] = subject.id
-              subject_detail["first_seq_mark"] = first_seq_mark
-              subject_detail["second_seq_mark"] = second_seq_mark
-              subject_detail["average_mark"] = average_mark
-              subject_detail["coefficient"] = subject.coefficient
-              subject_detail["score"] = score
-              subject_detail["grade"] = "To Do"
-              subject_detail["rank"] = student.sequence_rank(sequence_averages)
-              subject_detail["teacher"] = second_seq.teachers_name
-              subject_detail["remark"] = subject.add_remarks(average_mark)
-              # binding.break
-              details << subject_detail.to_s
-            else
-              [first_seq, second_seq].each do |s|
-                @warning_messages << generate_warning_message("Missing enrollment for student #{student.full_name}", s, s.seq_title(with_class_name: false)) unless student.was_enrolled?(s.hashed_marks)
+
+    begin
+      check_sequences_status
+
+      unless @has_unapproved_sequence
+        @students.each do |student|
+          next unless student.active?
+          total_score = 0
+          total_coefficient = 0
+          report_card_object = { school_id: @school.id, school_class_id: @school_class.id, term_id: @term.id,
+                                 student_id: student.id, academic_year_id: @academic_year.id, report_card_generator_id: @report_card_generator.id }
+          details = []
+          passed_subjects = 0
+          @subjects.each do |subject|
+            subject_detail = {}
+            first_seq, second_seq = subject.sequences.approved.where(term_id: @term.id, academic_year_id: @academic_year.id).order(:seq_num)
+            if first_seq.present? && second_seq.present?
+              if student.was_enrolled?(first_seq.hashed_marks) && student.was_enrolled?(second_seq.hashed_marks)
+                sequence_marks = @term.sum_sequence_subject_marks(subject.id)
+                sequence_averages = @term.calc_sequence_averages(sequence_marks)
+                first_seq_mark = student.sequence_mark_per_subject(first_seq.hashed_marks)
+                second_seq_mark = student.sequence_mark_per_subject(second_seq.hashed_marks)
+                total_coefficient += subject.coefficient
+                average_mark = (first_seq_mark + second_seq_mark) / 2
+                score = average_mark * subject.coefficient
+                total_score += score
+                passed_subjects += 1 if average_mark >= 10 # assuming that we are working on 20. In the future, passed mark could be a setting at the level of the school and class
+                subject_detail["name"] = subject.name
+                subject_detail["id"] = subject.id
+                subject_detail["first_seq_mark"] = first_seq_mark
+                subject_detail["second_seq_mark"] = second_seq_mark
+                subject_detail["average_mark"] = average_mark
+                subject_detail["coefficient"] = subject.coefficient
+                subject_detail["score"] = score
+                subject_detail["grade"] = Subject.grading(average_mark)
+                subject_detail["rank"] = student.sequence_rank(sequence_averages)
+                subject_detail["teacher"] = second_seq.teachers_name
+                subject_detail["remark"] = subject.add_remarks(average_mark)
+                # binding.break
+                details << subject_detail.to_s
+              else
+                [first_seq, second_seq].each do |s|
+                  @warning_messages << generate_warning_message("Missing enrollment for student #{student.full_name}", s, s.seq_title(with_class_name: false)) unless student.was_enrolled?(s.hashed_marks)
+                end
               end
+            else
+              @warning_messages << generate_warning_message("Missing Sequence for subject #{subject.title}", subject, subject.title)
             end
-          else
-            @failed_errors << generate_error_message("Missing Sequence for subject #{subject.title}", subject, subject.title)
           end
+
+          report_card_object[:total_score] = total_score
+          report_card_object[:total_coefficient] = total_coefficient
+          report_card_object[:average] = total_coefficient > 0 ? (total_score / total_coefficient) : 0
+          report_card_object[:passed_subjects] = passed_subjects
+          report_card_object[:details] = details
+          @bulk_report << report_card_object
         end
-
-        report_card_object[:total_score] = total_score
-        report_card_object[:total_coefficient] = total_coefficient
-        report_card_object[:average] = total_coefficient > 0 ? (total_score / total_coefficient) : 0
-        report_card_object[:passed_subjects] = passed_subjects
-        report_card_object[:details] = details
-        @bulk_report << report_card_object
       end
-    end
 
-    rank_report_card
-    add_other_attributes
+      rank_report_card
+      add_other_attributes
 
-    if @failed_errors.present?
-      process_duration = Time.now - start_time
-      @report_card_generator.update(failed_errors: @failed_errors.uniq,
-                                    warning_messages: @warning_messages.uniq, process_duration: process_duration,
-                                    is_successful: false)
-    else
       process_duration = Time.now - start_time
       reports_to_be_deleted = ReportCard.where(school_class_id: @school_class.id, term_id: @term.id, academic_year_id: @academic_year.id)
       reports_to_be_deleted.destroy_all if reports_to_be_deleted.present?
       ReportCard.insert_all @bulk_report
-      @report_card_generator.update(is_successful: true, warning_messages: @warning_messages,
+      @report_card_generator.update(is_successful: true, warning_messages: @warning_messages.uniq,
                                     process_duration: process_duration, student_num: @enrollment,
                                     student_passed_num: calc_student_passed_num, class_average: calc_class_average,
                                     most_performed_students: get_most_performed_students, failed_errors: @failed_errors,
@@ -237,10 +234,12 @@ class ReportCardGenerator < ApplicationRecord
       @report_card_generator.report_card_file.attach(io: pdf_path, filename: file_name)
       pdf_generator.delete_pdf_file
       process_duration = Time.now - start_time
-      @report_card_generator.update(progress_state: 4, process_duration: process_duration)
+      @report_card_generator.update(is_processing: false, progress_state: 4, process_duration: process_duration, is_successful: true)
+    rescue => exception
+      p exception
+      process_duration = Time.now - start_time
+      @report_card_generator.update(is_processing: false, process_duration: process_duration, is_successful: false)
     end
-
-    @report_card_generator.update(is_processing: false)
   end
 
   def self.generate_warning_message(title, object, object_name)
@@ -257,7 +256,7 @@ class ReportCardGenerator < ApplicationRecord
       @has_unapproved_sequence = true
       unapproved_seq = sequences.select { |s| !s.approved? }
       unapproved_seq.each do |s|
-        @failed_errors << generate_error_message("Unapproved Sequence for #{s.seq_title(with_class_name: true)}", s, s.seq_title(with_class_name: true))
+        @warning_messages << generate_warning_message("Unapproved Sequence for #{s.seq_title(with_class_name: true)}", s, s.seq_title(with_class_name: true))
       end
     end
   end
